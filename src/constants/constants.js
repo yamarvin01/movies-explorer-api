10683550ef22a8027109616ab40ec48ae807f5e9
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2023-02-25 19:07:54 +0300

    changes

--- src/constants/constants.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-useless-escape */
-const urlRegExp = /https?:\/\/(www\.)?[\w\-]+\.[\w\-]+\/*[\w\-\/\.\+\(\)\[\]~:?#@!$&'*,;=]*#?/;
-const emailRegExp = /[\w\-\_\.]+@[\w\-\_\.]+\.[\w\-\_\.]+/;
-
-module.exports = {
-  urlRegExp,
-  emailRegExp,
-};
